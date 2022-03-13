ec097d9bc5fabbaab1c46264bbf179683d2f2591
cameronjohnson30 <cameron_johnson91@icloud.com>
2022-03-12 22:21:43 -0800

    add mercurylife function with pass test, Bugfix mars test passing

--- __TESTS__/Mars.test.js
+++ __TESTS__/Mars.test.js
@@ -3,7 +3,7 @@ import Mars from '../src/Mars';
 describe  ('Mars' , () => {
 
   test( "should return Mars function" , () => {
-    let mars = new Mars(30);
+    let mars = new Mars(30, 30);
     expect(mars.planet).toEqual("Mars");
     expect(mars.age).toEqual(30);
     expect(mars.year).toEqual(1.88);
--- src/Mercury.js
+++ src/Mercury.js
@@ -1,13 +1,26 @@
 export default class Mercury {
-  constructor(age) {
+  constructor(age, expectancy) {
     this.planet = 'Mercury';
     this.age = age;
     this.year = .24;
     this.MercuryAge = 0;
+    this.expectancy = expectancy;
+    this.lifeClock = 0;
+
   }
   MercuryCalc() {
     let years = this.age / this.year; 
     return this.MercuryAge = this.MercuryAge + years;
   }
-}
+
+  MercuryLife() {
+    let death =this.age - this.expectancy; 
+    if (death > 0)
+      return this.lifeClock = death;
+    else if (death < 0)
+      return this.lifeClock = death * -1;
+    else (death === 0);
+    return this.lifeClock = death;
+  }
+  }
 
