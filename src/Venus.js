1dafb17a05fe53f4e99a5d092436157446b00db5
cameronjohnson30 <cameron_johnson91@icloud.com>
2022-03-12 22:31:30 -0800

    add venuslife function with passing tests

--- src/Venus.js
+++ src/Venus.js
@@ -1,9 +1,11 @@
 export default class Venus {
-  constructor(age) {
+  constructor(age, expectancy) {
     this.planet = 'Venus';
     this.age = age;
     this.year = .62;
     this.VenusAge = 0;
+    this.expectancy = expectancy;
+    this.lifeClock = 0; 
   }
 
   VenusCalc() {
@@ -11,4 +13,14 @@ export default class Venus {
     let planetAge = this.VenusAge + years;
     return this.VenusAge = Math.round(planetAge);
   }
-}
+
+  VenusLife() {
+    let death =this.age - this.expectancy; 
+    if (death > 0)
+      return this.lifeClock = death;
+    else if (death < 0)
+      return this.lifeClock = death * -1;
+    else (death === 0);
+    return this.lifeClock = death;
+  }
+  }
