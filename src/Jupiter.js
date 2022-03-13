bbfca5ee33ee1633f2765ab014f255a024f140ab
cameronjohnson30 <cameron_johnson91@icloud.com>
2022-03-12 19:26:49 -0800

    add passing test and code for jupiter

---
+++ src/Jupiter.js
@@ -0,0 +1,10 @@
+export default class Jupitar {
+  constructor(age, expectancy) {
+    this.planet = 'Jupiter';
+    this.age = age;
+    this.year = 11.86;
+    this.JupiterAge = 0;
+    this.expectancy = expectancy;
+    this.deathClock = 0; 
+  }
+}
